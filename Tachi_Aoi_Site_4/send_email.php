23843c31d5a8358d7977c8b416fe53f053b8be46
James Morton <james_morton@brown.edu>
2023-11-16 13:34:16 -0500

    Updating script to be simpler Fixing logic for email sending

--- Tachi_Aoi_Site_4/send_email.php
+++ Tachi_Aoi_Site_4/send_email.php
@@ -5,10 +5,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Ensure proper form field values and sanitize inputs
     $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+    $message = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+
+    $japaneseName = filter_var($_POST['japaneseName'], FILTER_SANITIZE_STRING);
+    $japaneseEmail = filter_var($_POST['japaneseEmail'], FILTER_SANITIZE_EMAIL);
+    $japaneseMessage = filter_var($_POST['japaneseSubject'], FILTER_SANITIZE_STRING);
 
     // Perform basic validation
-    if (empty($name) || empty($email) || empty($message)) {
+    if ((empty($name) || empty($email) || empty($message)) && (empty($japaneseName) || empty($japaneseEmail) || empty($japaneseMessage))) {
         echo "All fields are required.";
     } else {
         // Configure email settings
